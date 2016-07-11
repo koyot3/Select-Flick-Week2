6a4b169a11392d93d70ac3dfbbc34331f90b02d3
Hoang Nguyen <nguyenmanhhoang91@gmail.com>
2016-07-11 20:20:19 +0700

    WIP

--- Select_Flick_Week2/FavoriteMovieService.swift
+++ Select_Flick_Week2/FavoriteMovieService.swift
@@ -10,26 +10,56 @@ import Foundation
 import RealmSwift
 
 class FavoriteMovieService {
+
+    static func getFavoriteMovies(username: String) -> UserFavoriteMoviesDTO {
+        let realm = try! Realm()
+        let predicate = NSPredicate(format: "username = %@", username)
+        let favoriteMovies = realm.objects(UserFavoriteMoviesDTO.self).filter(predicate)
+        if favoriteMovies.count == 0 {
+            return UserFavoriteMoviesDTO()
+        } else {
+            return (favoriteMovies.first)!
+        }
+    }
     
     static func clickFavoriteButton(username: String, movieId: Int){
         let realm = try! Realm()
         let predicate = NSPredicate(format: "username = %@", username)
-        let favoriteMovies = realm.objects(FavoriteMovieDTO.self).filter(predicate)
+        let favoriteMovies = realm.objects(UserFavoriteMoviesDTO.self).filter(predicate)
+        realm.beginWrite()
         if favoriteMovies.count == 0 {
             // add new
-            var userFavMovies = FavoriteMovieDTO()
+            var userFavMovies = UserFavoriteMoviesDTO()
             userFavMovies.username = username
-            
-        } else {
-            var favoriteMovies = favoriteMovies.first?.favouriteMovies
-            // update
-            if favoriteMovies!.indexForKey(movieId) != nil {
-                favoriteMovies![movieId] = !(favoriteMovies![movieId]!)
-            } else {
-
+            var movie = FavoriteMovieDTO()
+            movie.movieId = movieId
+            movie.isLike = true
+            movie.transactionDate = NSDate()
+            userFavMovies.favouriteMovies.append(movie)
+            try! realm.write() {
+                realm.create(UserFavoriteMoviesDTO.self, value: userFavMovies)
             }
             
-            
+        } else {
+            var isFound:Bool = false
+            for movie in (favoriteMovies.first?.favouriteMovies)! {
+                let m = movie as! FavoriteMovieDTO
+                if m.movieId == movieId {
+                    m.isLike = !movie.isLike
+                    m.transactionDate = NSDate()
+                    isFound = true
+                }
+            }
+            if isFound == false {
+                let movie = FavoriteMovieDTO()
+                movie.movieId = movieId
+                movie.isLike = true
+                movie.transactionDate = NSDate()
+                favoriteMovies.first?.favouriteMovies.append(movie)
+            }
+            try! realm.write() {
+                realm.create(UserFavoriteMoviesDTO.self, value: (favoriteMovies.first?.favouriteMovies)!, update: true)
+            }
         }
     }
 }
